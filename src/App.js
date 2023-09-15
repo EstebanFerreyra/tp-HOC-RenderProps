1b0cbdf42b8d3854087c26a001238b2882e5ce3a
EstebanFerreyra <eferreyra098@gmail.com>
2023-09-15 01:09:02 -0300

    cambio en app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
 import Toggle from "./components/toggle/Toggle";
 import Tooltip from "./components/toolTip/ToolTip";
 
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <h1 className="mt-5">Beer Info</h1>
-//       <div className="row">
-//         <div className="col-md-6">
-//           <Toggle>
-//             <Tooltip text="Aca mostramos la informacion de la IPA">
-//               <h2 className="mt-3">IPA</h2>
-//             </Tooltip>
-//           </Toggle>
-//         </div>
-//         <div className="col-md-6">
-//           <Toggle>
-//             <Tooltip text="Aca mostramos la informacion de la Honey">
-//               <h2 className="mt-3">Honey</h2>
-//             </Tooltip>
-//           </Toggle>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// import React from 'react';
-
 const App = () => {
   return (
-    <div className="container mt-5">
+    <div className="container mt-3">
       <div className="card">
-        <div className="card-header">
+        <div className="card-header text-center">
           <h1>Beer Info</h1>
         </div>
         <div className="card-body">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-3">
               <Toggle>
                 <Tooltip text="Aca mostramos la informacion de la IPA">
                   <h2 className="mt-3">IPA</h2>
                 </Tooltip>
               </Toggle>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-3">
               <Toggle>
                 <Tooltip text="Aca mostramos la informacion de la Honey">
                   <h2 className="mt-3">Honey</h2>
+                </Tooltip>
+              </Toggle>
+            </div>
+            <div className="col-md-3">
+              <Toggle>
+                <Tooltip text="Aca mostramos la informacion de la Red">
+                  <h2 className="mt-3">Red</h2>
+                </Tooltip>
+              </Toggle>
+            </div>
+            <div className="col-md-3">
+              <Toggle>
+                <Tooltip text="Aca mostramos la informacion de la Irish">
+                  <h2 className="mt-3">Irish</h2>
                 </Tooltip>
               </Toggle>
             </div>
@@ -58,3 +46,5 @@ const App = () => {
 };
 
 export default App;
+
+
